4139b6f5512ea646d40d1dfc5d5472dbf2b80a99
Tommi S.E. Laukkanen <tommi.s.e.laukkanen@gmail.com>
2018-06-24 15:44:01 +0300

    Implementing terrain.

--- static/terrain.js
+++ static/terrain.js
@@ -21,8 +21,8 @@ AFRAME.registerComponent('terrain', {
      * Initial creation and setting of the mesh.
      */
     init: function () {
-        var data = this.data;
-        var el = this.el;
+        let data = this.data;
+        let el = this.el;
 
         // Create geometry.
         //this.geometry = new THREE.BoxBufferGeometry(data.width, data.height, data.depth);
@@ -36,11 +36,20 @@ AFRAME.registerComponent('terrain', {
         const dx = Math.cos(Math.PI / 3);
         const dy = Math.sin(Math.PI / 3);
 
-        var getVector3 = (i, j) => {
-            return new THREE.Vector3(i + j * dx, j * dy, 0)
+        const radius = 20.0;
+        const step = 1;
+
+        const radiusSquared = radius * radius;
+
+        let getHeight = (x, y) => {
+            return 2 * Math.sin(Math.PI * (x*x + y*y) / radiusSquared);
         }
 
-        var addFace = (i, j, step, v, primary) => {
+        let getVector3 = (i, j) => {
+            return new THREE.Vector3(i + j * dx, j * dy, getHeight(i,j))
+        };
+
+        let addFace = (i, j, step, v, primary) => {
             if (primary) {
                 this.geometry.vertices.push(
                     getVector3(i, j),
@@ -55,32 +64,49 @@ AFRAME.registerComponent('terrain', {
                 );
             }
 
-            var face = new THREE.Face3(v + 0, v + 1, v + 2);
-            face.vertexColors[0] = new THREE.Color(0xff0000); // red
-            face.vertexColors[1] = new THREE.Color(0x00ff00); // green
-            face.vertexColors[2] = new THREE.Color(0x0000ff); // blue
+            let face = new THREE.Face3(v + 0, v + 1, v + 2);
+            face.vertexColors[0] = new THREE.Color(0x00ff00);
+            face.vertexColors[1] = new THREE.Color(0x00ff00);
+            face.vertexColors[2] = new THREE.Color(0x00ff00);
             this.geometry.faces.push(face);
-        }
+        };
 
-        const radius = 10
-        const step = 0.5
-        var v = 0
-        for (var i = -radius; i < radius; i+= step) {
-            for (var j = -radius; j < radius; j+= step) {
+        let v = 0;
+        for (let i = -radius; i < radius; i+= step) {
+            for (let j = -radius; j < radius; j+= step) {
                 if (Math.abs(i + j) < radius) {
-                    addFace(i, j, step, v, true)
+                    addFace(i, j, step, v, true);
                     v += 3;
                 }
                 if (Math.abs(i + j + step) < radius) {
-                    addFace(i, j, step, v, false)
+                    addFace(i, j, step, v, false);
                     v += 3;
                 }
             }
         }
 
-        this.material = new THREE.MeshBasicMaterial({vertexColors: THREE.VertexColors});
+        this.geometry.mergeVertices();
+        this.geometry.computeFaceNormals();
+        this.geometry.computeVertexNormals();
+
+        //this.material = new THREE.MeshLambertMaterial({ color: 0x000000, shading: THREE.FlatShading, wireframe: true, transparent: true });
+        this.material = new THREE.MeshLambertMaterial({ color: 0xffffff, vertexColors: THREE.VertexColors });
         this.mesh = new THREE.Mesh(this.geometry, this.material);
         el.setObject3D('mesh', this.mesh);
+
+        this.wireMaterial = new THREE.MeshLambertMaterial({ color: 0xdddddd, wireframe: true, vertexColors: THREE.VertexColors });
+        this.wireMesh = new THREE.Mesh(this.geometry, this.wireMaterial);
+        el.setObject3D('wire', this.wireMesh);
+
+        /*
+        var materials = [
+            new THREE.MeshLambertMaterial({ color: 0xffffff, shading: THREE.FlatShading, vertexColors: THREE.VertexColors }),
+            new THREE.MeshBasicMaterial({ color: 0x000000, shading: THREE.FlatShading, wireframe: true, transparent: true })
+        ];
+        var group1 = THREE.SceneUtils.createMultiMaterialObject(this.geometry, materials);
+        group1.position.x = -400;
+        group1.rotation.x = -1.87;*/
+
     }
 });
 
